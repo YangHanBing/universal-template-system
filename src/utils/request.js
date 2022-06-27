b8d973a2bec8bc21af0e0884d19e93de72daf665
YangHanBing <2180244540@qq.com>
2022-06-27 16:47:14 +0800

    feat(axios): loading设置

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,20 +1,25 @@
 import axios from 'axios'
+import loading from './loading'
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
 })
 service.interceptors.request.use(
   (config) => {
+    loading.open()
     return config
   },
   (err) => {
+    loading.close()
     return Promise.reject(err)
   })
 service.interceptors.response.use(
   (res) => {
+    loading.close()
     return res
   },
   (err) => {
+    loading.close()
     return Promise.reject(err)
   })
 
