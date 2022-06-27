414e7e6f9406b04147088da4579a30c30c72197e
YangHanBing <2180244540@qq.com>
2022-06-27 14:09:51 +0800

    feat(登录页面): 密码框状态切换

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -27,7 +27,7 @@
       <el-form-item prop="password">
         <span class="svg-container">
           <el-icon>
-             <svg-icon icon="password"></svg-icon>
+            <svg-icon icon="password"></svg-icon>
           </el-icon>
         </span>
         <el-input :type="inputType" v-model="loginForm.password"></el-input>
@@ -48,7 +48,7 @@
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue'
+import { reactive, ref, computed } from 'vue'
 import { validatePassword } from './rule'
 // import { Avatar, Search, View, Hide } from '@element-plus/icons-vue'
 
@@ -76,6 +76,12 @@ const loginRules = reactive({
   ]
 })
 
+// 利用计算属性监听密码框状态
+const passwordIconStatus = computed(() => {
+  return inputType.value === 'password' ? 'eye' : 'eye-open'
+})
+
+// 登录事件
 const handleLoginSubmit = async (formName) => {
   if (!formName) return
   await formName.validate((valid) => {
@@ -85,7 +91,7 @@ const handleLoginSubmit = async (formName) => {
   })
 }
 
-// 密码框状态切换
+// 点击密码框状态切换
 const handllePassWordStatus = () => {
   inputType.value = inputType.value === 'password' ? 'text' : 'password'
 }
@@ -97,34 +103,34 @@ $dark_gray: #889aa4;
 $light_gray: #eee;
 $cursor: #fff;
 
-.login-container{
+.login-container {
   position: relative;
   height: 100%;
-  background-color:$bg;
+  background-color: $bg;
 
-  .login-form{
+  .login-form {
     width: 520px;
     padding: 0 35px;
-    position :absolute;
-    left : 50%;
-    margin-left : -260px;
-    top : 160px;
+    position: absolute;
+    left: 50%;
+    margin-left: -260px;
+    top: 160px;
     overflow: hidden;
 
-    ::v-deep .el-form-item{
+    ::v-deep .el-form-item {
       border: 1px solid rgba(255, 255, 255, 0.1);
       background: rgba(0, 0, 0, 0.1);
       border-radius: 5px;
       color: #454545;
 
-      .svg-container{
+      .svg-container {
         padding: 6px 5px 6px 15px;
         color: $dark_gray;
         vertical-align: middle;
         display: inline-block;
       }
 
-      .svg-pwd{
+      .svg-pwd {
         position: absolute;
         right: 20px;
         top: 10px;
@@ -136,52 +142,52 @@ $cursor: #fff;
     }
 
     ::v-deep .el-input {
-        display: inline-block;
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+      .el-input__wrapper {
+        background: transparent !important;
+        box-shadow: none;
+      }
+      .el-input__wrapper.is-focus {
+        box-shadow: none;
+      }
+      input {
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
         height: 47px;
-        width: 85%;
-        .el-input__wrapper{
-          background: transparent!important;
-          box-shadow: none;
-        }
-        .el-input__wrapper.is-focus{
-          box-shadow: none;
-        }
-        input {
-          border: 0px;
-          -webkit-appearance: none;
-          border-radius: 0px;
-          padding: 12px 5px 12px 15px;
-          color: $light_gray;
-          height: 47px;
-          caret-color: $cursor;
-        }
-      }
-
-    .title-container{
-      position: relative;
-
-      .title{
-        font-size: 26px;
-        color : $light_gray;
-        text-align: center;
-        font-weight: bold;
-        margin-bottom : 40px;
-      }
-      ::v-deep .svg-language{
-          position: absolute;
-          top: 4px;
-          right: 0;
-          background-color: #fff;
-          font-size: 22px;
-          padding: 4px;
-          border-radius: 4px;
-          cursor: pointer;
+        caret-color: $cursor;
       }
     }
 
-    .login-button{
-      width : 100%;
-      margin-bottom : 30px;
+    .title-container {
+      position: relative;
+
+      .title {
+        font-size: 26px;
+        color: $light_gray;
+        text-align: center;
+        font-weight: bold;
+        margin-bottom: 40px;
+      }
+      ::v-deep .svg-language {
+        position: absolute;
+        top: 4px;
+        right: 0;
+        background-color: #fff;
+        font-size: 22px;
+        padding: 4px;
+        border-radius: 4px;
+        cursor: pointer;
+      }
+    }
+
+    .login-button {
+      width: 100%;
+      margin-bottom: 30px;
     }
   }
 }
