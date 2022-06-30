bde31d8a139e4903090f6b7f1a79de09dbdf9a73
YangHanBing <2180244540@qq.com>
2022-06-30 14:03:35 +0800

    feat(api): 封装获取首页用户信息接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,5 @@
 import request from '@/utils/request'
+// 登录接口
 const login = (data) => {
   return request({
     url: '/sys/login',
@@ -6,6 +7,14 @@ const login = (data) => {
     data
   })
 }
+// 获取首页用户信息
+const getUserInfo = () => {
+  return request({
+    url: '/sys/profile',
+    method: 'GET'
+  })
+}
 export default {
-  login
+  login,
+  getUserInfo
 }
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>个人中心区</div>
+  <div class="appmain">
+    <div class="user">个人中心</div>
+    <div class="desc"></div>
+  </div>
 </template>
-<script setup>
-
-</script>
-<style scoped lang="scss"></style>
+<script setup></script>
+<style scoped lang="scss">
+.appmain {
+  width:100%;
+  overflow:auto;
+  display:flex;
+  .user {
+    width:25%;
+  }
+  .desc{
+    width:70%;
+  }
+}
+</style>
